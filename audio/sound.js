1bd052fca58b9ae5938ca0cf451ab57712f89c1a
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-08-24 19:08:30 +0900

    [add] 音声再生を追加

--- audio/sound.js
+++ audio/sound.js
@@ -3,3 +3,7 @@ emojiau.soundData = {};
 emojiau.createSound = (id,url) => {
     emojiau.soundData[id] = new Tone.Player(url).toDestination();
 }
+
+emojiau.playSound = (id) => {
+    emojiau.soundData[id].start();
+}
