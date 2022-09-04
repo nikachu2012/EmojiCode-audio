6d3d0856b435871ac2a59a3e743d1c82180aa696
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-09-04 19:16:37 +0900

    [update] ビルドデータの更新

--- emojiaudio.js
+++ emojiaudio.js
@@ -1,7 +1,7 @@
 /* 
     EmojiCode Sprite Controller
     Created by nikachu2012(https://github.com/nikachu2012)
-    Create time: Wed Aug 24 2022 19:20:26 GMT+0900 (日本標準時)
+    Create time: Sun Sep 04 2022 19:13:49 GMT+0900 (日本標準時)
 */
 //create a synth and connect it to the main output (your speakers)
 const synth = new Tone.Synth().toDestination();
@@ -17,9 +17,20 @@ emojiau.playNoteSecond = (note, second) => {
 }
 
 emojiau.soundData = {};
+emojiau.soundDetail = {};
 
 emojiau.createSound = (id,url) => {
     emojiau.soundData[id] = new Tone.Player(url).toDestination();
+    emojiau.soundDetail[id] = {}
+    emojiau.soundDetail[id].id = id;
+    emojiau.soundDetail[id].url = url;
+}
+
+emojiau.detailAdd = (detail) => {
+    const keys = Object.keys(detail);
+    keys.forEach(element => {
+        emojiau.createSound(detail[element].id, detail[element].url)
+    });
 }
 
 emojiau.playSound = (id) => {
