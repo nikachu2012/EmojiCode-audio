81dbe9049a6b8d0d270c60d90c8101408c32f1ae
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-09-04 17:38:10 +0900

    [add] セーブされたファイルを読み込める関数を追加

--- audio/sound.js
+++ audio/sound.js
@@ -1,7 +1,18 @@
 emojiau.soundData = {};
+emojiau.soundDetail = {};
 
 emojiau.createSound = (id,url) => {
     emojiau.soundData[id] = new Tone.Player(url).toDestination();
+    emojiau.soundDetail[id] = {}
+    emojiau.soundDetail[id].id = id;
+    emojiau.soundDetail[id].url = url;
+}
+
+emojiau.detailAdd = (detail) => {
+    const keys = Object.keys(detail);
+    keys.forEach(element => {
+        emojiau.createSound(detail[element].id, detail[element].url)
+    });
 }
 
 emojiau.playSound = (id) => {
