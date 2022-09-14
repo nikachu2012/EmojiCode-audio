54e065fe44917f0851fb55cf172c14fb4dc82a4a
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-09-15 00:00:45 +0900

    [add] 音IDにnullが利用できないように変更

--- audio/sound.js
+++ audio/sound.js
@@ -2,10 +2,16 @@ emojiau.soundData = {};
 emojiau.soundDetail = {};
 
 emojiau.createSound = (id,url) => {
-    emojiau.soundData[id] = new Tone.Player(url).toDestination();
-    emojiau.soundDetail[id] = {}
-    emojiau.soundDetail[id].id = id;
-    emojiau.soundDetail[id].url = url;
+    if(id !== null){
+        emojiau.soundData[id] = new Tone.Player(url).toDestination();
+        emojiau.soundDetail[id] = {}
+        emojiau.soundDetail[id].id = id;
+        emojiau.soundDetail[id].url = url;
+    }
+    else{
+        console.error('emojiau: IDにnullは使用できません。')
+        alert('IDにnullは使用できません。')
+    }
 }
 
 emojiau.detailAdd = (detail) => {
